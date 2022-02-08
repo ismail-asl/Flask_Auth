95497b2fe4c90e544b0d24bed691a95627c88ea8
ismail-asl <ismail@aslgroup.com.bd>
2022-02-08 18:00:44 +0600

    token incomplete

--- app2.py
+++ app2.py
@@ -93,13 +93,19 @@ def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
 
-        token = request.args.get('token')
+        token = None
 
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
 
         if not token:
             return jsonify({'message':"Token in missing"}, 403)
+
         try:
             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            current_user = User.query.filter_by(public_id=data['public_id'])
+
+             
         except Exception as e:
             print(e)
             return jsonify({'message':"Token is invalid"}, 403)
